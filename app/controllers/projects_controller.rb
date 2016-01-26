3f9e34dda0c4fcc1e062930fc4f613398a39cc71
RemiVR <remi.vr5@gmail.com>
2016-01-26 17:51:10 +0100

    edit and delete projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,9 +4,6 @@ class ProjectsController < ApplicationController
 		@projects = Project.all
 	end
 
-	def show
-		@project = Project.find(params[:id])
-	end
 
 	def new
 		@project = Project.new
@@ -21,7 +18,34 @@ class ProjectsController < ApplicationController
 		end
 	end
 
+	def show
+		@project = Project.find(params[:id])
+	end
+
+	def edit
+		@project = Project.find(params[:id])
+	end
+
+	def update
+		@project = Project.find(params[:id])	
+
+		if @project.update(project_params)
+			redirect_to @project
+		else	
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@project = Project.find(params[:id])
+		@project.destroy
+
+		redirect_to root_path
+	end
+
+
 	private
+
 		def project_params
 			params.require(:project).permit(:title, :link, :description)
 		end
